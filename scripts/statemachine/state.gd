1e3fe12ac240471175316dcf84067623871145c1
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-08-18 00:29:39 -0700

    remove translations

--- scripts/statemachine/state.gd
+++ scripts/statemachine/state.gd
@@ -1,3 +1,21 @@
 class_name State
 extends Node
 
+func handle_input(_event: InputEvent) -> void:
+	pass
+
+
+func update(_delta: float) -> void:
+	pass
+
+
+func physics_update(_delta: float) -> void:
+	pass
+
+
+func enter(_msg := {}) -> void:
+	pass
+
+
+func exit() -> void:
+	pass
