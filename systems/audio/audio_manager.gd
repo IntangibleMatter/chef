8ca61c14dc2ccf0188ce66140cd105b38ad3b6f8
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-08-19 22:50:26 -0700

    audio singleton

---
+++ systems/audio/audio_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var current_music_event : EventInstance
+var current_music_alt_event : EventInstance
+var current_ambient_event : EventInstance
+
+
--- tests/audio/fmodtest.gd
+++ tests/audio/fmodtest.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up"):
 		print(';asdkj')
-		var inst := RuntimeManager.create_instance_path("event:/sfx/veg/cuke/cukeattacktest")
+		var inst := RuntimeManager.create_instance_id("event:/obj/veg/cuke/cukeattacktest")
 		print(inst)
 		RuntimeManager.attach_instance_to_node(inst, self)
 		print(RuntimeManager.attached_instances)
