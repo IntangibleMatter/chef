4b856ca0e75549307cea23894495a56447057ac9
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-09-10 12:20:15 -0700

    cadsfasdf

--- game.gd
+++ game.gd
@@ -1,7 +1,26 @@
 extends Node
 
+var curr_scene : Node
 var current_scene_path : String
 
+signal scene_spawned(scene: String)
+
 func change_scene(scene: String, msg : Dictionary = {}) -> void:
-	pass
+	# TODO: This should be moved to a thread, to avoid lag spikes. Shouldn't be too much of an issue.
+	var scn : Node
+	
+	if FileAccess.is_path_valid(scene):
+		scn = load(scene).instantiate()
+	else:
+		return
+	
+	if curr_scene_hold.get_child_count() > 0:
+		for child in curr_scene_hold.get_children():
+			child.queue_free()
+	
+	curr_scene_hold.add_child(scn)
+	curr_scene = scn
+	current_scene_path = scene
+
+	print(scn)
 
--- systems/cutscene/resources/cutscene_resource.gd
+++ systems/cutscene/resources/cutscene_resource.gd
@@ -1,13 +1,26 @@
-class_name Cutscene
+class_name Cutscene_DEPRECATED
 extends Resource
 
+## So, this was better a while back, but I decided that to make everybody's
+## (read: my) lives easier, I'd just make it so all the actors are in one giant
+## Dict loaded on launch. Bit of a performance hit but I doubt that'll be an
+## issue. If it is, well fuck me I guess. Refactoring is future Lyric's problem.
+## And, at the time of writing this, I'm present Lyric. Fuck that future guy.
+## Well, no, that's inaccurate and a bit mean. I hope he's doing well :) Hey
+## girl, how're you feeling, is she certain she's trans yet? If you are I'm sorry
+## for deadnaming you if that's not your actual name now. Uhhh... yeah, anyways.
+## Cutscenes are loaded as plain-text .cut files. End of story.
+##
+## tl;dr Write your cutscenes in files that end with [code].cut[/code]
+
+"""
 enum CUTSCENE_FROM { TEXT, FILE }
 
-@export var characters : Array[Character]
+@export var characters : Array[Actor]
 
 @export_subgroup("Contents")
 @export var text_type : CUTSCENE_FROM = CUTSCENE_FROM.TEXT
 ## Can be used as either the text which the cutscene is built from, or the path
 ## to a text file which the cutscene is built from. Base on the parameter above.
 @export_multiline var contents : String
- 
+""" 
